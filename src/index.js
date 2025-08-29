4801aa610a64abeefe7963a0390289dc92dd9183
wjdgotjd28-del <wjdgotjd28@gmail.com>
2025-08-29 09:01:12 +0900

    8-28

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './propsDrilling/App';
-
+import DarkOrLight from './chapter_14/DarkOrLight';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
